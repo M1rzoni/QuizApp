02e956bb7b7e9fb700bdfe41c126013c7f9aef27
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-03-28 13:38:15 +0100

    "debugging"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import Header from './components/Header';
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import "./components/style/Header.css";
+import questions from "../Questions.js";
 
+function App() {
+  const [counter, setCounter] = useState(60);
+  const [randomQuestion, setRandomQuestion] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [count, setCount] = useState(0);
 
-function App ()  {
+  const handleNextQuestion = () => {
+    setCount((prevIndex) => (prevIndex + 1) % questions.length);
+    document.querySelectorAll(".buttons-container button").forEach((button) => {
+      button.classList.remove("green");
+      button.classList.remove("red");
+    });
 
+    setCounter(60);
+  };
+
+  useEffect(() => {
+    const randomQuestion = questions[count];
+    const options = randomQuestion?.options || [];
+    setRandomQuestion(randomQuestion);
+    setOptions(options);
+  }, [count]);
+
+  useEffect(() => {
+    setCounter(60);
+  }, [randomQuestion]);
+
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+
+    if(counter <= 0){
+      alert("You got out of the time")
+    }
+  }, [counter]);
 
   return (
     <div>
-      <Header  />
+      <Header
+        value={counter}
+        randomQuestion={randomQuestion}
+        options={options}
+        question={questions[count]}
+        onNextQuestion={handleNextQuestion}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,41 @@
-import "./style/Header.css";
-import questions from "../../Questions";
-import { useTimer } from "react-timer-hook";
-
-
-function Header() {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  const randomQuestion = questions[randomIndex];
-  const options = randomQuestion?.options || [];
+function Header(props) {
+  const { randomQuestion, options, value, onNextQuestion, } = props;
 
   const handleAnswer = (index) => {
     const clickedAnswer = options[index];
     const correctAnswer = randomQuestion.correct_answer;
 
     if (Array.isArray(correctAnswer)) {
-        if (correctAnswer.includes(clickedAnswer)) {
-            document.getElementById(`button_${index}`).classList.add("green");
-        } else {
-            document.getElementById(`button_${index}`).classList.add('red');
-        }
+      if (correctAnswer.includes(clickedAnswer)) {
+        document.getElementById(`button_${index}`).classList.add("green");
+      } else {
+        document.getElementById(`button_${index}`).classList.add("red");
+      }
     } else {
-        if (clickedAnswer === correctAnswer) {
-            document.getElementById(`button_${index}`).classList.add("green");
-            alert("Good job u got 1 point!")
-        } else {
-            document.getElementById(`button_${index}`).classList.add("red");
-            alert("Wrong!")
-        }
+      if (clickedAnswer === correctAnswer) {
+        document.getElementById(`button_${index}`).classList.add("green");
+        alert("Good job u got 1 point!");
+      } else {
+        document.getElementById(`button_${index}`).classList.add("red");
+        alert("Wrong!");
+      }
     }
-};
-
+  };
 
   return (
     <div className="container">
       <div className="quiz-container">
         <div className="header-container">
           <h2>Quiz</h2>
-          <p>Time: 60s</p>
+          <p>{value}</p>
         </div>
         <div className="question-container">
-          <h1>{randomQuestion.question}</h1>
+          <h1>{randomQuestion?.question}</h1>
         </div>
         <div className="buttons-container">
           {options.map((option, index) => (
             <button
-              id={`button_${index}`} 
+              id={`button_${index}`}
               key={index}
               onClick={() => handleAnswer(index)}
             >
@@ -53,7 +45,7 @@ function Header() {
         </div>
         <div className="next-container">
           <p>2 of 9 Question</p>
-          <button>Next</button>
+          <button onClick={onNextQuestion}>Next</button>
         </div>
       </div>
     </div>
